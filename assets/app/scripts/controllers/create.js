cdcd6aeda70bca917eb8f2870866cdfb6682519c
Samuel Padgett <spadgett@redhat.com>
2015-09-15 14:18:04 -0400

    [WIP] Reorganize web console create flow

--- assets/app/scripts/controllers/create.js
+++ assets/app/scripts/controllers/create.js
@@ -9,24 +9,24 @@
  */
 angular.module('openshiftConsole')
   .controller('CreateController', function ($scope, DataService, tagsFilter, uidFilter, hashSizeFilter, imageStreamTagAnnotationFilter, LabelFilter, $location, Logger) {
-    var projectImageStreams,
-        openshiftImageStreams,
+    var projectImageRepos,
+        openshiftImageRepos,
         projectTemplates,
         openshiftTemplates;
 
-    // The tags to use for categories in the order we want to display. Empty string corresponds to the "Other" category.
+    // The tags to use for categories in the order we want to display. The
+    // empty string corresponds to the "Other" category.
     $scope.categoryTags = [
-      "instant-app", "xpaas", "java", "php", "ruby", "perl", "python", "nodejs", "database", "messaging", ""
+      "instant-app", "java", "php", "ruby", "perl", "python", "nodejs", "database", "messaging", ""
     ];
 
     $scope.categoryLabels = {
       "instant-app": "Instant Apps",
       java: "Java",
-      xpaas: "xPaaS",
       php: "PHP",
       ruby: "Ruby",
-      perl: "Perl",
       python: "Python",
+      perl: "Perl",
       nodejs: "NodeJS",
       database: "Databases",
       messaging: "Messaging",
@@ -43,9 +43,7 @@ angular.module('openshiftConsole')
     // Set to false if there is data to show.
     $scope.emptyCatalog = true;
 
-    $scope.filterByTag = function(tag) {
-      $scope.searchTerm = tag;
-    };
+    $scope.sourceURLPattern = /^((ftp|http|https|git):\/\/(\w+:{0,1}[^\s@]*@)|git@)?([^\s@]+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
 
     // List templates in the project namespace as well as the shared
     // `openshift` namespace.
@@ -53,26 +51,30 @@ angular.module('openshiftConsole')
       projectTemplates = templates.by("metadata.name");
       angular.forEach(projectTemplates, categorizeTemplate);
       updateState();
+      Logger.info("project templates", projectTemplates);
     });
 
     DataService.list("templates", {namespace: "openshift"}, function(templates) {
       openshiftTemplates = templates.by("metadata.name");
       angular.forEach(openshiftTemplates, categorizeTemplate);
       updateState();
+      Logger.info("openshift templates", openshiftTemplates);
     });
 
     // List image streams in the project namespace as well as the shared
     // `openshift` namespace.
-    DataService.list("imagestreams", $scope, function(imageStreams) {
-      projectImageStreams = imageStreams.by("metadata.name");
-      imagesForStreams(projectImageStreams);
+    DataService.list("imagestreams", $scope, function(imageRepos) {
+      projectImageRepos = imageRepos.by("metadata.name");
+      imagesForRepos(projectImageRepos);
       updateState();
+      Logger.info("project image repos", projectImageRepos);
     });
 
-    DataService.list("imagestreams", {namespace: "openshift"}, function(imageStreams) {
-      openshiftImageStreams = imageStreams.by("metadata.name");
-      imagesForStreams(openshiftImageStreams);
+    DataService.list("imagestreams", {namespace: "openshift"}, function(imageRepos) {
+      openshiftImageRepos = imageRepos.by("metadata.name");
+      imagesForRepos(openshiftImageRepos);
       updateState();
+      Logger.info("openshift image repos", openshiftImageRepos);
     });
 
     function addImageToCategory(image, category) {
@@ -83,17 +85,17 @@ angular.module('openshiftConsole')
       $scope.builderImagesByCategory[category].push(image);
     }
 
-    function imagesForStreams(imageStreams) {
-      angular.forEach(imageStreams, function(imageStream) {
-        if (!imageStream.status) {
+    function imagesForRepos(imageRepos) {
+      angular.forEach(imageRepos, function(imageRepo) {
+        if (!imageRepo.status) {
           return;
         }
 
         // Create a map of spec tags so we can find them efficiently later when
         // looking at status tags.
         var specTags = {};
-        if (imageStream.spec && imageStream.spec.tags) {
-          angular.forEach(imageStream.spec.tags, function(tag) {
+        if (imageRepo.spec && imageRepo.spec.tags) {
+          angular.forEach(imageRepo.spec.tags, function(tag) {
             if (tag.annotations && tag.annotations.tags) {
               specTags[tag.name] = tag.annotations.tags.split(/\s*,\s*/);
             }
@@ -101,15 +103,15 @@ angular.module('openshiftConsole')
         }
 
         // Loop over status tags to categorize the images.
-        angular.forEach(imageStream.status.tags, function(tag) {
-          var imageStreamTag = tag.tag;
+        angular.forEach(imageRepo.status.tags, function(tag) {
+          var imageRepoTag = tag.tag;
           var image = {
-            imageStream: imageStream,
-            imageStreamTag: imageStreamTag,
-            name: imageStream.metadata.name + ":" + imageStreamTag,
-            version: imageStreamTagAnnotationFilter(imageStream, 'version', imageStreamTag)
+            imageRepo: imageRepo,
+            imageRepoTag: imageRepoTag,
+            name: imageRepo.metadata.name + ":" + imageRepoTag,
+            version: imageStreamTagAnnotationFilter(imageRepo, 'version', imageRepoTag)
           };
-          var category, categoryTags = specTags[imageStreamTag] || [];
+          var category, categoryTags = specTags[imageRepoTag] || [];
           if (categoryTags.indexOf("builder") >= 0) {
             // Add the builder image to its category.
             category = getCategory(categoryTags);
@@ -128,7 +130,7 @@ angular.module('openshiftConsole')
       // Find the first category that is in tags.
       for (i = 0; i < $scope.categoryTags.length; i++) {
         for (j = 0; j < tags.length; j++) {
-          if (tags[j].toLowerCase() === $scope.categoryTags[i]) {
+          if (tags[j] === $scope.categoryTags[i]) {
             return tags[j];
           }
         }
@@ -154,8 +156,8 @@ angular.module('openshiftConsole')
       $scope.loaded =
         projectTemplates &&
         openshiftTemplates &&
-        projectImageStreams &&
-        openshiftImageStreams;
+        projectImageRepos &&
+        openshiftImageRepos;
 
       if ($scope.loaded) {
         Logger.info("templates by category", $scope.templatesByCategory);
